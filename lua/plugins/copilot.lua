f3117b80fd9e2d06f4d7d317908a20ca05136ded
Michael Efemena <mefemena@gmail.com>
2023-12-12 15:07:23 +0100

    init

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {}
+--return { { "github/copilot.vim" } }
+
+--return {
+--{
+--"zbirenbaum/copilot.lua",
+--cmd = "Copilot",
+--event = "InsertEnter",
+--config = function()
+--require("copilot").setup({ suggestion = { auto_trigger = true, keymap = {} } })
+--end,
+--},
+--}
