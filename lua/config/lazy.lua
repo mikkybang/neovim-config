d6a0630f02580f113486c4a7781390b753954a14
Michael Efemena <mefemena@gmail.com>
2024-01-21 19:43:19 +0100

    change color scheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
