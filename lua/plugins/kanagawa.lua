8b3f1cccb7d9d1dc1d69158fe0d8fe6406a990bd
Michael Efemena <mefemena@gmail.com>
2024-06-10 22:02:58 +0100

    chore: add new theme and telescope plugin

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,10 @@
+return {
+  -- Kanagawa colorscheme
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("kanagawa").setup({ theme = "lotus" })
+      vim.cmd.colorscheme("kanagawa")
+    end,
+  },
+}
